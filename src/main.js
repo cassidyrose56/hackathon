0ad68f32f11ec3502bed320cc249b1a1624a05d8
Cassidy Johnson <cassidyrose56@gmail.com>
2022-09-28 19:20:07 -0500

    first commit of hackathon, completed some work on manifest

---
+++ src/main.js
@@ -0,0 +1,54 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const ninja = document.createElement('img');
+//     ninja.setAttribute('id', 'ninja');
+//     ninja.setAttribute('src', "./src/assets/Ninja-no-bg.png");
+//     document.getElementById('body').appendChild('ninja');
+// });
+
+
+
+
+
+
+document.addEventListener('click', () => {
+  var test = document.querySelectorAll('.MV3Tnb');
+  console.log("I am clicking!!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// to add to the scripts manifest "jquery.min.js", 
+
+
+//JavaScript
+// var body = document.querySelectorAll('.MV3Tnb')
+// var contents = document.getElementById('contents');
+// contents.parentNode.removeChild(contents);
+
+// jQuery
+// var contents = $('#contents');
+// contents.remove();
